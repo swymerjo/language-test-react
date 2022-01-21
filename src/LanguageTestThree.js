2c3e6b64c3de76819b2ca94fc63e5740ba57eb91
swymerjo <sammyjowymerpsych@gmail.com>
2022-01-21 16:43:47 +0100

    added third questions section

---
+++ src/LanguageTestThree.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { questionsThree } from './questionsThree';
+
+export default function LanguageTestTwo(scoreTwo) {
+	const currentScore = scoreTwo;
+	console.log(currentScore.scoreTwo);
+	const [indexLanguageTest, setIndexLanguageTest] = useState(0);
+	const { question, answerOptions, questionNumber, id } =
+		questionsThree[indexLanguageTest];
+	const [score, setScore] = useState(currentScore.scoreTwo);
+	const [testComplete, setTestComplete] = useState(false);
+
+	function handleAnswerClickTest(isCorrect) {
+		console.log(isCorrect);
+		console.log(score);
+		if (isCorrect) {
+			setScore(score + 1);
+			console.log(score);
+			nextQuestionTest();
+		} else {
+			nextQuestionTest();
+		}
+	}
+
+	function nextQuestionTest() {
+		let nextQuestionTest = id + 1;
+		console.log(nextQuestionTest);
+		if (nextQuestionTest < questionsThree.length) {
+			setIndexLanguageTest(nextQuestionTest);
+		} else {
+			setTestComplete(true);
+		}
+	}
+
+	return (
+		<div className="language-test-container">
+			<section className="sub-question-container">
+				<h3>
+					In this section you must choose the word which best fits each space in
+					the text below.
+				</h3>
+				<hr className="line"></hr>
+				<h4 className="sub-question-text">
+					Alice Guy Blaché Alice Guy Blaché was the first female film director.
+					She first became involved in cinema whilst working for the German Film
+					Company in the late 1890s. This was a period of great change in the
+					cinema and Alice was the first to use many new inventions, ____ sound
+					and color. In 1907 Alice ____ to New York where she started her own
+					film company. She was ____ successful, but, when Hollywood became the
+					center of the film world, the best days of the independent New York
+					film companies were ____. When Alice died in 1968, hardly anybody ____
+					her name.
+				</h4>
+			</section>
+
+			<h3 className="language-test-question sub-question-text">
+				{questionNumber}. {question}
+			</h3>
+			<div className="answer-section" key={questionNumber}>
+				{answerOptions.map((answerOption, index) => (
+					<button
+						key={index}
+						onClick={() => handleAnswerClickTest(answerOption.isCorrect)}
+						className="answer-btn"
+					>
+						{answerOption.answerText}
+					</button>
+				))}
+				<p>Question {questionNumber}/30</p>
+				<p>Your score is {score}</p>
+			</div>
+		</div>
+	);
+}
--- src/LanguageTestTwo.js
+++ src/LanguageTestTwo.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { questionsTwo } from './questionsTwo';
+import LanguageTestThree from './LanguageTestThree';
 
 export default function LanguageTestTwo(scoreOne) {
 	const currentScore = scoreOne;
@@ -33,41 +34,51 @@ export default function LanguageTestTwo(scoreOne) {
 	}
 
 	return (
-		<div className="language-test-container">
-			<section className="sub-question-container">
-				<h3>
-					In this section you must choose the word which best fits each space in
-					the text below.
-				</h3>
-				<hr className="line"></hr>
-				<h4 className="sub-question-text">
-					Scotland is the north part of the island of Great Britain. The
-					Atlantic Ocean is on the west and the North Sea on the east. Some
-					people ____ Scotland speak a different language called Gaelic. There
-					are ____ five million people in Scotland, and Edinburgh is ____ most
-					famous city. Scotland has many mountains; the highest one is called
-					'Ben Nevis'. In the south of Scotland, there are a lot of sheep. A
-					long time ago, there ____ many forests, but now there are only a ____.
-					Scotland is only a small country, but it is quite beautiful.
-				</h4>
-			</section>
+		<>
+			{testComplete ? (
+				<section className="language-test-two">
+					<LanguageTestThree scoreTwo={score} />
+					{/* <TestResults score={score} courses={courses}/> */}
+				</section>
+			) : (
+				<div className="language-test-container">
+					<section className="sub-question-container">
+						<h3>
+							In this section you must choose the word which best fits each
+							space in the text below.
+						</h3>
+						<hr className="line"></hr>
+						<h4 className="sub-question-text">
+							Scotland is the north part of the island of Great Britain. The
+							Atlantic Ocean is on the west and the North Sea on the east. Some
+							people ____ Scotland speak a different language called Gaelic.
+							There are ____ five million people in Scotland, and Edinburgh is
+							____ most famous city. Scotland has many mountains; the highest
+							one is called 'Ben Nevis'. In the south of Scotland, there are a
+							lot of sheep. A long time ago, there ____ many forests, but now
+							there are only a ____. Scotland is only a small country, but it is
+							quite beautiful.
+						</h4>
+					</section>
 
-			<h3 className="language-test-question sub-question-text">
-				{questionNumber}. {question}
-			</h3>
-			<div className="answer-section" key={questionNumber}>
-				{answerOptions.map((answerOption, index) => (
-					<button
-						key={index}
-						onClick={() => handleAnswerClickTest(answerOption.isCorrect)}
-						className="answer-btn"
-					>
-						{answerOption.answerText}
-					</button>
-				))}
-				<p>Question {questionNumber}/30</p>
-				<p>Your score is {score}</p>
-			</div>
-		</div>
+					<h3 className="language-test-question sub-question-text">
+						{questionNumber}. {question}
+					</h3>
+					<div className="answer-section" key={questionNumber}>
+						{answerOptions.map((answerOption, index) => (
+							<button
+								key={index}
+								onClick={() => handleAnswerClickTest(answerOption.isCorrect)}
+								className="answer-btn"
+							>
+								{answerOption.answerText}
+							</button>
+						))}
+						<p>Question {questionNumber}/30</p>
+						<p>Your score is {score}</p>
+					</div>
+				</div>
+			)}
+		</>
 	);
 }
