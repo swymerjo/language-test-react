af3627af8995931054b1f01ef009c3a9d9bf6eb8
swymerjo <sammyjowymerpsych@gmail.com>
2022-01-31 14:33:09 +0100

    added links and style to footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,19 +2,27 @@ import React from 'react';
 
 export default function Footer() {
 	return (
-		<span className="footer-container">
-			<p className="footer-p">
-				Property of the
-				<a href="" alt="csh website" className="footer-link-csh">
-					{' '}
-					CSH{' '}
-				</a>
-				created by
-				<a href="" alt="sammy wymer" className="footer-link-sammy">
-					{' '}
-					Sammy Wymer
-				</a>
-			</p>
-		</span>
+		<div className="footer-container">
+			<span>
+				<p className="footer-p">
+					Property of the
+					<a
+						href="http://www.carl-schurz-haus.de/home.html"
+						alt="csh website"
+						className="footer-link-csh"
+					>
+						CSH
+					</a>
+					created by
+					<a
+						href="https://sammy-wymer.netlify.app"
+						alt="sammy wymer"
+						className="footer-link-sammy"
+					>
+						Sammy Wymer
+					</a>
+				</p>
+			</span>
+		</div>
 	);
 }
